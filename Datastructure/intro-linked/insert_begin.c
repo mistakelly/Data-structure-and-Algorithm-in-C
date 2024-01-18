5a5748636934c2390d3334afeebbabf57e2d0d3d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-18 23:00:00 +0100

    DSA

--- Datastructure/intro-linked/insert_begin.c
+++ Datastructure/intro-linked/insert_begin.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+struct Node
+
 /**
  * @brief: main function
  * its the driver function of the program
@@ -11,5 +13,5 @@
 int main(int argc, char *argv[])
 {
 	/*creation of node*/
-	
+
 }
