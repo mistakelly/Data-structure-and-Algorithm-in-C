473c2c3c43b987676ddc24d2a9ce28f1f74ba12a
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-23 23:19:59 +0100

    working

--- Datastructure/intro-linked/revision/insert_begin.c
+++ Datastructure/intro-linked/revision/insert_begin.c
@@ -21,7 +21,7 @@ int kelly(void)
 
     struct Node *node1 = (struct Node *)malloc(sizeof(struct Node));
     struct Node *node2 = (struct Node *)malloc(sizeof(struct Node));
-    struct Node* node3 = (struct Node *)malloc(sizeof(nOD))
+    struct Node* node3 = (struct Node *)malloc(sizeof(struct Node))
     node1->data = 5;
     
     node1->next = node2;
