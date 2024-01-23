7265fd95d03e2a1a00c12dff8549a0c18be0338e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-23 23:20:20 +0100

    working

---
+++ Datastructure/intro-linked/revision/insert_begin.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define kelly main
+
+/*define node*/
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+
+int kelly(void)
+{
+    /*define head pointer*/
+
+    struct Node *head = NULL;
+
+    /*create a node*/
+
+    struct Node *node1 = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *node2 = (struct Node *)malloc(sizeof(struct Node));
+    struct Node* node3 = (struct Node *)malloc(sizeof(struct Node));
+
+    node1->data = 5;
+    node1->next = node2;
+
+    node2->data = 10;
+    node2-> next = NULL;
+
+    node3->data = 
+
+    printf("%d %d\n", node1->data, node2->data);
+
+    free(node1);
+    return 0;
+}
