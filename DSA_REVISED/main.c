9c84191eb91d4c3ffcb845bab40c9c000b1ce16a
kelvin okoye <kelvinokoye413@gmail.com>
2024-03-05 19:28:59 +0000

    lists

--- DSA_REVISED/main.c
+++ DSA_REVISED/main.c
@@ -13,16 +13,18 @@ int count_nodes(struct Node *head)
 {
 
     struct Node* current = NULL;
-    current = head;
+    // current = *head;
+
+
 
     int count = 0;
 
 
-    while (current != NULL)
+    while (head != NULL)
     {
         /* code */
         count++;
-        current = current -> next;
+        head = head -> next;
     }    
 
     return count;
@@ -33,16 +35,46 @@ int main(int argc, char *argv[])
 
     struct Node* head = NULL;
     struct Node* node1 = NULL;
+    struct Node* node2 = NULL;
+    struct Node* node3 = NULL;
+    struct Node* node4 = NULL;
 
+
+
+   
+   
+
+    
     // create list and allocate memory.
      node1 = (struct Node*)malloc(sizeof(struct Node));
+     node2 = (struct Node*)malloc(sizeof(struct Node));
+     node3 = (struct Node*)malloc(sizeof(struct Node));
+     node4 = (struct Node*)malloc(sizeof(struct Node));
 
+    head = node1;
+    int count = count_nodes(head);
+    
      node1->data = 5;
-     node1->next = NULL;
+     node1->next = node2;
+
+     node2->data = 10;
+     node2->next = node3;
+
+     node3->data = 15;
+     node3->next = node4;
+
+     node4->data = 20;
+     node4->next = NULL;
 
      printf("%d\n", node1->data);
-
+     printf("%d\n", node2->data);
+     printf("%d\n", node3->data);
+     printf("%d\n", node4->data);
+    //  printf("sizeof nodes <%d>\n", count);
      free(node1);
+     free(node2);
+     free(node3);
+     free(node4);
 
     return 0;
 }
