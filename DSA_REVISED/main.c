4ca85ce42ba6f2d7b2ce99ad43c0e22eedff2b9f
kelvin okoye <kelvinokoye413@gmail.com>
2024-03-05 18:59:59 +0000

    linked list

--- DSA_REVISED/main.c
+++ DSA_REVISED/main.c
@@ -1,19 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-char* func_string(char *string)
+struct Node
 {
-    string[0] = 'o';
-    return string;
+    /* data */
+    int data;
+    struct Node *next;
+};
 
+
+int count_nodes(struct Node *head)
+{
+
+    struct Node* current = head;
+
+    int count = 0;
+
+
+    while (current != NULL)
+    {
+        /* code */
+        count++;
+        current = current -> next;
+    }    
+
+    return count;
+    
 }
-
 
 int main(int argc, char *argv[])
 {
 
-   s
+    struct Node* head = NULL;
+    struct Node* node1 = NULL;
+
+    // create list and allocate memory.
+     node1 = (struct Node*)malloc(sizeof(struct Node));
+
+     node1->data = 5;
+     node1->next = NULL;
+
+     printf("%d\n", node1->data);
+
+     free(node1);
 
     return 0;
 }
